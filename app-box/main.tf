d02ce9e8ae2100b6e2f25674d89ce3314c57d7e5
Jayant Kaushal <jayant.kaushal@aisera.com>
2022-06-30 16:54:22 +0530

    Updating config

--- app-box/main.tf
+++ app-box/main.tf
@@ -3,15 +3,3 @@ module "helm_chart" {
   namespace = var.namespace
   helm_chart_list = var.chart_configurations
 }
-module "grafana_folders" {
-  source = "./terraform_components/modules/apps/grafana-folder"
-  grafana_folders = var.grafana_folders
-}
-module "grafana_data_source" {
-  source = "./terraform_components/modules/apps/grafana-datasource"
-  grafana_data_sources = var.grafana_data_sources
-}
-module "grafana-dashboard" {
-  source = "./terraform_components/modules/apps/grafana-dashboard"
-  grafana_dashboards = var.grafana_dashboards
-}
