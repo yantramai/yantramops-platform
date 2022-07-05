4aeb13626456d1640cf8c52c9759189496354d98
Jayant Kaushal <jayant.kaushal@aisera.com>
2022-07-05 11:57:02 +0530

    Setting up Django -3

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {})
