6f4159b51bdf05c2dd850b75063db46b20cc0d44
Jayant Kaushal <jayant.kaushal@aisera.com>
2022-07-07 11:12:32 +0530

    adding some prometheus colors

--- deployment/promql_engine/postgres_metrices.py
+++ deployment/promql_engine/postgres_metrices.py
@@ -2,7 +2,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 import json
 
-from com.yantram.core.promql_engine.promql_exec import PromqlExec
+from deployment.promql_engine.promql_exec import PromqlExec
 
 
 class postgres_metrices :
